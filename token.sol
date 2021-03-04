2c41666dba31633278c7df412e4440beddc71e82
BigBroKuang <45906268+BigBroKuang@users.noreply.github.com>
2021-03-03 18:48:31 -0600

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,86 +1,80 @@
 // SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0;
 
-pragma solidity ^0.4.16;
-
-interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes memory _extraData) public; }
-
-contract TokenERC20 {
-    string public name; 
-    string public symbol;
-    uint8 public decimals=18;  // 18 is recommended
-    uint256 public totalSupply;
-
-    mapping (address => uint256) public balanceOf;  // the balance of the account
-    mapping (address => mapping (address => uint256)) public allowance; //addr1 allows addr2 to spend uint256 tokens
-
-    event Transfer(address indexed from, address indexed to, uint256 value); //record the transaction on ETH chain
-    event Burn(address indexed from, uint256 value); //otherwise, the transactions are recorded btween contract and sender
-
-    //basics of the token
-    function ERCToken(uint256 initialSupply, string memory tokenName, string memory tokenSymbol) public {
-        totalSupply = initialSupply * 10 ** uint256(decimals); //min unit as in ethereum
-        balanceOf[msg.sender] = totalSupply;  //the amount of token in the owner's account
-        name = tokenName;                     //token name (e.g. chainlink)
-        symbol = tokenSymbol;                 //token symbol(e.g. LINK)
-    }
-
-    //an internal function, define the transaction in the token system
-    function _transfer(address _from, address _to, uint _value) internal {   //sender, recipient, amount
-        assert(balanceOf[_from] >= _value);         //sender has enough balance
-        assert(balanceOf[_to] + _value > balanceOf[_to]);   //to verify there is no overflow
-        uint previousBalances = balanceOf[_from] + balanceOf[_to];  // balances of the 2 accounts before the transaction
-        balanceOf[_from] -= _value;   //reduce the balance in sender's account
-        balanceOf[_to] += _value;     //increase the balance in recipient's account 
-        emit Transfer(_from, _to, _value);    //emit an event when there is a transaction
-        assert(balanceOf[_from] + balanceOf[_to] == previousBalances);  //double check the balances of the 2 accounts
-    }
-    
-    //this function calls the internal transaction function
-    function transfer(address _to, uint256 _value) public {
-        _transfer(msg.sender, _to, _value);   //the message sender is the sender
-    }
-
-     //transfer token with delegate's account
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        //addr1 has enough balance. addr2 initiates the transaction
-        require(_value <= allowance[_from][msg.sender]);     
-        allowance[_from][msg.sender] -= _value;   //decrease the allowance of addr2
-        _transfer(_from, _to, _value);    //initiate the transaction with addr1
-        return true;
-    }
-
-    //addr1 approves addr2 to spend addr1's token
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-        allowance[msg.sender][_spender] = _value;   //the allowance for addr2
-        return true;
-    }
-    
-    
-    function approveAndCall(address _spender, uint256 _value, bytes memory _extraData) public returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
-            return true;
-        }
-    }
-   
-    //to burn some tokens for a user
-    function burn(uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value); //the amount to burn is less than the balance 
-        balanceOf[msg.sender] -= _value;    //burn the amount token for the user
-        totalSupply -= _value;               //total supply has to be changed
-        emit Burn(msg.sender, _value);       //emit an event
-        return true;
-    }
-
-    //addr2 burns some tokens for addr1
-    function burnFrom(address _from, uint256 _value) public returns (bool success) {
-        require(balanceOf[_from] >= _value);   //the amount to burn is less than the balance 
-        require(_value <= allowance[_from][msg.sender]);  //allowance for addr2 is greater than the amount
-        balanceOf[_from] -= _value;   //burn the tokens for addr
-        allowance[_from][msg.sender] -= _value;  //reduce the amount in allowance
-        totalSupply -= _value;   //reduce the total supply
-        emit Burn(_from, _value);  //emit an event
-        return true;
-    }
+interface Token{
+    function balanceOf(address _owner)  external view returns (uint256 balance);
+    function transfer(address _to, uint256 _value) external returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool);
+    function approve(address _spender, uint256 _value) external returns (bool);
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256  _value);
 }
+
+contract TokenABC is Token{
+    uint256 public totalSupply;
+    string public name;                   //name of the token
+    uint8 public decimals=18;               //decimals 
+    string public symbol;               //symbol
+    mapping (address => uint256) balances;   //balance of the address, hash table
+    mapping (address => mapping (address => uint256)) allowed;    //addr1 allows addr2 to spend addr1's tokens
+
+    //initialize the token
+    constructor(uint256 _initialSupply, string memory _tokenName, string memory _tokenSymbol) {
+        totalSupply = _initialSupply * 10 ** uint256(decimals);         // initial supply
+        balances[msg.sender] = totalSupply; // balance of owner is the total supply
+        name = _tokenName;               //input the name    
+        symbol = _tokenSymbol;      
+    }
+    
+    //make transaction
+    function transfer(address _to, uint256  _value) external override returns (bool success) {
+        require(balances[msg.sender] >= _value);   //sender has enough balance
+        require(balances[_to] + _value > balances[_to]);  //there is no overflow in recipient's account
+        balances[msg.sender] -= _value;   //reduce the amount from sender's account
+        balances[_to] += _value;    //increase the amount in recipient's account
+        emit Transfer(msg.sender, _to, _value);  //emit the transfer event
+        return true;   
+    }
+    
+    //make transaction for delegate
+    function transferFrom(address _from, address _to, uint256 _value) external override returns (bool success) {
+        require(balances[_from] >= _value);  //addr1 has enough balance
+        require(allowed[_from][msg.sender] >= _value);  //addr2 has enough allowance from addr1
+        balances[_to] += _value; // increase recipient's balance
+        balances[_from] -= _value; //reduce sender's balance
+        allowed[_from][msg.sender] -= _value;  //reduce the allowed amount
+        emit Transfer(_from, _to, _value);  //emit the transfer event
+        return true;
+    }
+    
+    //addr1 approves addr2 to transfer addr1's tokens
+    function approve(address _spender, uint256 _value) external override returns (bool success)   
+    { 
+        allowed[msg.sender][_spender] = _value;    //addr1 delegate the right to addr2 with token amount 
+        emit Approval(msg.sender, _spender, _value);  //trigger the event
+        return true;
+    }
+    
+    //the balance of an account 
+    function balanceOf(address _owner) external view override returns (uint256 balance) {
+        return balances[_owner];   //return the balance of an account
+    }
+    
+    //the allowance of an account 
+    function allowance(address _owner, address _spender) external view override returns (uint256 remaining) {
+        return allowed[_owner][_spender]; //allowance of addr2 from addr1
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
